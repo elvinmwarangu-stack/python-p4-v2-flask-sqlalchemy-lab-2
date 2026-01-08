821b081dfc7684137eba69f41064743cbccee32e
Elvin <elvin.mwarangu@student.moringaschool.com>
2026-01-08 14:58:01 +0300

    done

--- server/models.py
+++ server/models.py
@@ -1,32 +1,79 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
-
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+# naming convention for migrations
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
+# ------------------ MODELS ------------------ #
+
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    # relationships
+    reviews = relationship('Review', back_populates='customer')
+
+    # association proxy (Task #2)
+    items = association_proxy('reviews', 'item')
+
+    # serialization rules (Task #3)
+    serialize_rules = (
+        '-reviews.customer',
+    )
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    # relationships
+    reviews = relationship('Review', back_populates='item')
+
+    # serialization rules (Task #3)
+    serialize_rules = (
+        '-reviews.item',
+    )
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    # relationships
+    customer = relationship('Customer', back_populates='reviews')
+    item = relationship('Item', back_populates='reviews')
+
+    # serialization rules (Task #3)
+    serialize_rules = (
+        '-customer.reviews',
+        '-item.reviews',
+    )
+
+    def __repr__(self):
+        return f'<Review {self.id}>'
